e330b0ed31f2bb1ff34e78adc654926cea692fe2
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-11-12 14:15:57 -0300

    arm vm

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -18,19 +18,19 @@ resource "azurerm_network_security_group" "default" {
   resource_group_name = var.resource_group_name
 }
 
-resource "azurerm_network_security_rule" "inbound" {
-  name                        = "allow-inbound"
-  priority                    = 1000
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = var.resource_group_name
-  network_security_group_name = azurerm_network_security_group.default.name
-}
+# resource "azurerm_network_security_rule" "inbound" {
+#   name                        = "allow-inbound"
+#   priority                    = 1000
+#   direction                   = "Inbound"
+#   access                      = "Allow"
+#   protocol                    = "*"
+#   source_port_range           = "*"
+#   destination_port_range      = "*"
+#   source_address_prefix       = "*"
+#   destination_address_prefix  = "*"
+#   resource_group_name         = var.resource_group_name
+#   network_security_group_name = azurerm_network_security_group.default.name
+# }
 
 resource "azurerm_network_security_rule" "outbound" {
   name                        = "allow-outbound"
